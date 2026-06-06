520277bffe01e178ac43fd28d829593ea65aca99
Hariharan <hari@Hariharans-MacBook-Air.local>
2026-06-06 16:03:45 +0530

    category components

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Categories from "./components/Categories";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import { useState, useEffect } from "react";
 
 function App() {
@@ -175,41 +175,9 @@ function App() {
           ))}
         </div>
       </section>
-{/* contact section */}
+<Contact/>
 
-<section className="contact-section">
-  <div className="contact-left">
-    <h2>Contact 555 Shoes</h2>
-
-    <p>📞 +91 7868905884</p>
-    <p>﹫ 555shoesindia@gmail.com</p>
-    <p>📍Garage no.19 <br />28th Cross Street <br /> Besant Nagar <br />Chennai-90</p>
-    <p>🕒 Open Daily:11 AM - 10 PM</p>
-
-    <button className="whatsapp-btn">
-      WhatsApp Us 
-    </button>
-  </div>
-
-  <div className="contact-right">
-    <iframe
-      title="555 Shoes Location"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.5524890068677!2d80.26728847588005!3d13.000449814243474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5267b91c1b1adb%3A0x8f09138cfc6dfb2e!2s555%20Shoes!5e0!3m2!1sen!2sin!4v1780142628710!5m2!1sen!2sin" 
-      width="100%"
-      height="400"
-      style={{ border: 0 }}
-      loading="lazy"
-    ></iframe>
-  </div>
-</section>
-
-{ /* footer */ }
-
-<footer className="footer">
-  <p>© 2026 555 Shoes</p>
-  <p>Every Step Has A Story</p>
-</footer>;
-
+<Footer />
     </div>
     
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer">
+        <p>© 2026 555 Shoes</p>
+        <p>Every Step Has A Story</p>
+      </footer>
+    );
+  }
+export default Footer
